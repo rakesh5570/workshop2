a5dc1c9857599a028ca308eba933fde2fccf95c8
rakesh5570 <honeyrakeshraj@gmail.com>
2022-08-01 09:28:30 +0530

    Added: Workshop2

---
+++ test2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+echo "Enter the number-> "
+read no
+
+i=1
+ans=0
+
+for i in 1 2 3 4 5 .. no
+do
+	if [[ $(( no%i )) -eq 0 ]]
+	then
+		ans=$(( ans+i ))
+fi
+i='expr $i+1'
+done
+
+if [ $no -eq $ans ]
+then
+	echo "$no is perfect "
+	else
+	echo "$no is Not perfect number"
+fi
